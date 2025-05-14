349917211fe4ac00f062283f945f9ef63aaccd7f
Piyush Thapliyal <piyushthapliyalprof@gmail.com>
2025-05-14 23:23:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import path from 'path'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': "	http://backend.authmate.xyz/",  // Replace with your backend server URL
+      '/api': "http://backend.authmate.xyz/",  // Replace with your backend server URL
     }
   },
   plugins: [react()],
